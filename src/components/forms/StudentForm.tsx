b4d0fa5df77024e1e0a9e6596f65ebf2098c915c
david victor <38535954+victor297@users.noreply.github.com>
2024-10-16 10:21:33 +0100

    Update StudentForm.tsx

--- src/components/forms/StudentForm.tsx
+++ src/components/forms/StudentForm.tsx
@@ -105,7 +105,7 @@ const StudentForm = ({
         Personal Information
       </span>
       <CldUploadWidget
-        uploadPreset="school"
+        uploadPreset="veeshop"
         onSuccess={(result, { widget }) => {
           setImg(result.info);
           widget.close();
