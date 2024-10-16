193bc832b518736c35cac218b3dc76254b772947
david victor <38535954+victor297@users.noreply.github.com>
2024-10-16 10:21:09 +0100

    Update TeacherForm.tsx

--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -184,7 +184,7 @@ const TeacherForm = ({
           )}
         </div>
         <CldUploadWidget
-          uploadPreset="school"
+          uploadPreset="veeshop"
           onSuccess={(result, { widget }) => {
             setImg(result.info);
             widget.close();
